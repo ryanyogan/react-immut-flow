d7b737f312108caec30926695db5c200041f0a4c
Ryan Yogan <ryanyogan@gmail.com>
2015-10-09 11:54:24 -0500

    added a page layout component

--- public/bundle.js
+++ public/bundle.js
@@ -19004,6 +19004,10 @@ var _main_header = require('./main_header');
 
 var _main_header2 = _interopRequireDefault(_main_header);
 
+var _page = require('./page');
+
+var _page2 = _interopRequireDefault(_page);
+
 var App = (function (_React$Component) {
   _inherits(App, _React$Component);
 
@@ -19019,7 +19023,16 @@ var App = (function (_React$Component) {
       return _react2['default'].createElement(
         'div',
         { className: 'app' },
-        _react2['default'].createElement(_main_header2['default'], null)
+        _react2['default'].createElement(_main_header2['default'], null),
+        _react2['default'].createElement(
+          _page2['default'],
+          null,
+          _react2['default'].createElement(
+            'h1',
+            null,
+            'Page Title'
+          )
+        )
       );
     }
   }]);
@@ -19030,7 +19043,7 @@ var App = (function (_React$Component) {
 exports['default'] = App;
 module.exports = exports['default'];
 
-},{"./main_header":159,"react":157}],159:[function(require,module,exports){
+},{"./main_header":159,"./page":161,"react":157}],159:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -19150,6 +19163,53 @@ module.exports = exports['default'];
 },{"react":157}],161:[function(require,module,exports){
 'use strict';
 
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var Page = (function (_React$Component) {
+  _inherits(Page, _React$Component);
+
+  function Page() {
+    _classCallCheck(this, Page);
+
+    _get(Object.getPrototypeOf(Page.prototype), 'constructor', this).apply(this, arguments);
+  }
+
+  _createClass(Page, [{
+    key: 'render',
+    value: function render() {
+      return _react2['default'].createElement(
+        'div',
+        { className: 'page' },
+        this.props.children
+      );
+    }
+  }]);
+
+  return Page;
+})(_react2['default'].Component);
+
+exports['default'] = Page;
+module.exports = exports['default'];
+
+},{"react":157}],162:[function(require,module,exports){
+'use strict';
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 var _react = require('react');
@@ -19166,4 +19226,4 @@ var _componentsApp2 = _interopRequireDefault(_componentsApp);
 
 _reactDom2['default'].render(_react2['default'].createElement(_componentsApp2['default'], null), document.querySelector("#app"));
 
-},{"./components/app":158,"react":157,"react-dom":2}]},{},[161]);
+},{"./components/app":158,"react":157,"react-dom":2}]},{},[162]);
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import MainHeader from './main_header';
+import Page from './page';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='app'>
         <MainHeader />
+        <Page>
+          <h1>Page Title</h1>
+        </Page>
       </div>
     );
   }

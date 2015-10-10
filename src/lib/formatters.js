b28ee33340b026d0211761f0c8d375759c1c2397
Ryan Yogan <ryanyogan@gmail.com>
2015-10-10 14:39:32 -0500

    added static type checking to formatters

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -1,15 +1,17 @@
+/* @flow */
 import numeral from 'numeral';
 import moment from 'moment';
+const Moment = moment().constructor;
 
-const toMoney = (num) => {
+const toMoney = (num: number) : string => {
   return numeral(num).format('$0,0.00');
 };
 
-const toDateTime = (mom) => {
+const toDateTime = (mom: Moment) : string => {
   return mom.format('MMM D, h:mma');
 };
 
-const toTitleCase = (str) => {
+const toTitleCase = (str: string) : string => {
   return str.replace(/\w\S*/g, (txt) => {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
